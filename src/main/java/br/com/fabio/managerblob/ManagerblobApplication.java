1efffe1eafdd60cb54d8e7de6d2ffc8bf2e1c44e
Fábio Damas <fabio.damas@gmail.com>
2022-12-19 08:46:08 -0300

    first commit

---
+++ src/main/java/br/com/fabio/managerblob/ManagerblobApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabio.managerblob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerblobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerblobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabio/managerblob/ManagerblobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabio.managerblob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerblobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
